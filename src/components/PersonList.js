6909704e4a848fa207eabf785f04fa45464ac4bf
Larissa Huang <larissa.huang@enginedigital.com>
2019-04-27 14:43:27 -0700

    retrieved job listings from GitHub Jobs API and set up npm cache adapter

--- src/components/PersonList.js
+++ src/components/PersonList.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import axios from 'axios';
+import { setupCache } from 'axios-cache-adapter'
+
+//create cache adaptor instance
+const cache = setupCache({
+    maxAge: 15 * 60 * 1000
+})
+
+// create axios instance passing the newly created cache adapter
+const api = axios.create({
+    adapter: cache.adapter
+})
+
 
 export default class PersonList extends React.Component {
     state = {
-        persons: []
+        jobs: []
     }
 
     componentDidMount() {
-        axios.get(`https://jsonplaceholder.typicode.com/users`).then(res => {
+        api({
+            // Send a GET request to some REST api and cache it
+            url: `${'https://cors-anywhere.herokuapp.com/'}https://jobs.github.com/positions.json?description=python&location=new+york`,
+            method: 'get'
+
+        }).then(async (res) => {
+
             console.log(res)
-            this.setState({ persons: res.data });
+
+            // Interacting with the store, see `localForage` API.
+            const length = await cache.store.length()
+            console.log('Cache:', cache);
+
+            console.log('Cache store length:', length)
+
+            // axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://jobs.github.com/positions.json?description=python&location=new+york`, { crossdomain: true }).then(res => {
+            //     console.log(res)
+            this.setState({ jobs: res.data });
         });
     }
 
     render() {
         return (
             <ul>
-                {this.state.persons.map(person => <li>{person.name}</li>)}
+                {this.state.jobs.map(job => <li>{job.title}</li>)}
+
             </ul>)
 
     }
